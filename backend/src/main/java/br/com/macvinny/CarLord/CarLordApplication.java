45f1ecbca92173324f41f050d24b01f9b8f67f2e
MacVinny <macvinny@protonmail.com>
2022-12-31 10:06:12 -0300

    start backend

---
+++ backend/src/main/java/br/com/macvinny/CarLord/CarLordApplication.java
@@ -0,0 +1,13 @@
+package br.com.macvinny.CarLord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLordApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/macvinny/CarLord/CarLordApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.macvinny.CarLord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
